3e553e805b699dbc070965bcce9202c7517879f0
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-02-04 14:29:47 +0900

    init: backend

---
+++ backend/src/test/java/com/rejs/nearlib/NearlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rejs.nearlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
