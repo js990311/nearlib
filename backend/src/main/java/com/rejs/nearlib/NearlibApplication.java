04b6e94f2f03ff38ee10d1bd07ae323d3892c209
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-02-04 14:53:18 +0900

    init: backend

---
+++ backend/src/main/java/com/rejs/nearlib/NearlibApplication.java
@@ -0,0 +1,13 @@
+package com.rejs.nearlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearlibApplication.class, args);
+	}
+
+}
