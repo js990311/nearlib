1b8555b002ea44cdf53fc13232d0fdc280f8de31
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-02-05 20:12:21 +0900

    style: sm size nav

--- frontend/app/components/Nav.tsx
+++ frontend/app/components/Nav.tsx
@@ -1,6 +1,11 @@
+"use client"
+
 import Link from "next/link";
+import {useState} from "react";
 
 export default function Nav():React.ReactNode {
+    const [openNav, setOpenNav] = useState(false);
+
     return (
         <nav>
             <div className="container mx-auto flex justify-between items-center">
@@ -23,7 +28,39 @@ export default function Nav():React.ReactNode {
                         TODO 내 정보 보기
                     </div>
                 </div>
+                <div>
+                    <button className="md:hidden" onClick={() => {
+                        setOpenNav(true);
+                    }}>
+                        열기
+                    </button>
+                </div>
             </div>
+            {/* small size nav side */}
+            {openNav && (
+                <div className="fixed inset-0 bg-white z-10">
+                    <div className="flex flex-col ">
+                        <div className="relative w-full py-3">
+                            <button
+                                className="absolute top-0 right-0"
+                                onClick={() => {
+                                    setOpenNav(false)
+                                }}
+                            >
+                                닫기
+                            </button>
+                        </div>
+                        <div>
+                            TODO 내 정보 보기
+                        </div>
+                        {/* TODO 검색창 */}
+                        <input type="text"/>
+                        <Link href={"/library"}>
+                            내 주변 도서관 찾기
+                        </Link>
+                    </div>
+                </div>
+            )}
         </nav>
     )
 }
