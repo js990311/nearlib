319e3bca8fe753d6be60f5c6925c900d69e097a6
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-02-05 21:08:51 +0900

    component: IconButton
    and add fontawsome

---
+++ frontend/app/components/button/IconButton.tsx
@@ -0,0 +1,16 @@
+type IconButtonProps = {
+    iconClass: string;
+    className?: string;
+    onClick?: () => void;
+}
+
+export default function IconButton({className, iconClass, onClick}: IconButtonProps):React.ReactNode {
+    return (
+        <button
+            className={className}
+            onClick={onClick}
+        >
+            <i className={iconClass}></i>
+        </button>
+    )
+}
--- frontend/app/components/nav/Nav.tsx
+++ frontend/app/components/nav/Nav.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import {useState} from "react";
+import IconButton from "@/app/components/button/IconButton";
 
 export default function Nav():React.ReactNode {
     const [openNav, setOpenNav] = useState(false);
@@ -9,31 +10,34 @@ export default function Nav():React.ReactNode {
     return (
         <nav>
             <div className="container mx-auto flex justify-between items-center">
-                <div id="nav-left" className="flex justify-between items-center">
+                <div id="nav-left" className="flex justify-start items-center">
                     <div className="md:mr-20">
-                        <Link href={"/"}>
+                        <Link href={"/public"}>
                             TODO logo
                         </Link>
                     </div>
-                    <div className="hidden md:flex justify-between items-center">
+                    <div className="hidden md:flex justify-start items-center">
                         <Link href={"/library"}>
                             내 주변 도서관 찾기
                         </Link>
                     </div>
                 </div>
-                <div id="nav-right" className="hidden md:flex justify-between items-center">
+                <div id="nav-right" className="hidden md:flex justify-end items-center">
                     {/* TODO 검색창 */}
                     <input type="text"/>
                     <div>
-                        TODO 내 정보 보기
+                        <IconButton
+                            iconClass="fa-solid fa-user p-3"
+                        />
                     </div>
                 </div>
-                <div>
-                    <button className="md:hidden" onClick={() => {
-                        setOpenNav(true);
-                    }}>
-                        열기
-                    </button>
+                <div className="md:hidden">
+                    <IconButton
+                        iconClass="fa-solid fa-bars"
+                        className="p-3"
+                        onClick={() => {
+                            setOpenNav(true);
+                    }} />
                 </div>
             </div>
             {/* small size nav side */}
@@ -41,14 +45,13 @@ export default function Nav():React.ReactNode {
                 <div className="fixed inset-0 bg-white z-10">
                     <div className="flex flex-col ">
                         <div className="relative w-full py-3">
-                            <button
+                            <IconButton
+                                iconClass="fa-solid fa-xmark p-3"
                                 className="absolute top-0 right-0"
                                 onClick={() => {
                                     setOpenNav(false)
                                 }}
-                            >
-                                닫기
-                            </button>
+                            />
                         </div>
                         <div>
                             TODO 내 정보 보기
