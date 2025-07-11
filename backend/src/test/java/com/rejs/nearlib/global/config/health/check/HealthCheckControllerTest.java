3cf0acd11a8e1839161bfdfc0e717b52722f7ebb
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-07-11 21:56:16 +0900

    test: HealthCheckController

---
+++ backend/src/test/java/com/rejs/nearlib/global/config/health/check/HealthCheckControllerTest.java
@@ -0,0 +1,25 @@
+package com.rejs.nearlib.global.config.health.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthCheckController.class)
+class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getHealthCheck() throws Exception{
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("OK"));
+    }
+}
