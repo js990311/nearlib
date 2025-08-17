e6f900dc99309f2c7cfd4fac28867b7c95fb51be
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-08-17 20:47:45 +0900

    feat: RangeRemoteController의 슬라이더 삭제

--- frontend/components/map/sidebar/RangeRemoteController.tsx
+++ frontend/components/map/sidebar/RangeRemoteController.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import {useState} from "react";
+import IconButton from "@/components/button/IconButton";
+
 type RangeRemoteControllerProps = {
     range: number,
     setRange : (range: number) => void,
@@ -7,38 +10,69 @@ type RangeRemoteControllerProps = {
 }
 
 export default function RangeRemoteController({range, setRange, onSearch}: RangeRemoteControllerProps) {
+    const [temp, setTemp] = useState<number>(100);
 
     return (
         <div
-            className={"text-blue-500"}
+            className={"text-green-50 border border-green-900 bg-green-500 rounded p-2"}
         >
             <div
                 className={"w-full"}
             >
-                {/* 주 리모컨 */}
-                <label htmlFor="range-slider">범위 (m 단위)</label>
-                <input type="range"
-                       min={"100"}
-                       max={"500000"}
-                       value={range}
-                       onChange={(e) => {
-                           setRange(Number(e.target.value));
-                       }}
-                />
-                <input type="number"
-                       value={range}
-                       onChange={(e) => {
-                           setRange(Number(e.target.value));
-                       }}
-                />
-                <button
-                    onClick={onSearch}
+                <h3
+                    className={"text-lg font-bold"}
                 >
-                    검색하기
-                </button>
+                    검색 범위 (m단위)
+                </h3>
+                <div>
+                    <input type="number"
+                           className={"bg-green-700 rounded p-2"}
+                           value={range}
+                           onChange={(e) => {
+                               setRange(Number(e.target.value));
+                           }}
+                    />
+                    <button
+                        className={"bg-blue-500 rounded p-2"}
+                        onClick={onSearch}
+                    >
+                        검색하기
+                    </button>
+                </div>
+                <div>
+                    <div className={"mt-1"}>
+                        <IconButton
+                            iconClass={"fa-solid fa-minus"}
+                            onClick={() => {
+                                setRange(range - temp);
+                            }}
+                        />
+                        <input type="number"
+                           className={"bg-green-700 rounded"}
+                            value={temp}
+                            onChange={(e) => {
+                                setTemp(Number(e.target.value));
+                            }}
+                        />
+                        <IconButton
+                            iconClass={"fa-solid fa-plus"}
+                            onClick={() => {
+                                setRange(range + temp);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <button onClick={()=>{setTemp(100)}}>100m</button>
+                        <button onClick={()=>{setTemp(500)}}>500m</button>
+                        <button onClick={()=>{setTemp(1000)}}>1km</button>
+                        <button onClick={()=>{setTemp(3000)}}>3km</button>
+                        <button onClick={()=>{setTemp(5000)}}>5km</button>
+                        <button onClick={()=>{setTemp(10000)}}>10km</button>
+                    </div>
+                </div>
             </div>
             <div>
-            {/*    버튼형 리모컨  */}
+                {/*    버튼형 리모컨  */}
             </div>
         </div>
     )
